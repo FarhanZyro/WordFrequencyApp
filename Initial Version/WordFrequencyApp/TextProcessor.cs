ddf25d0105df031dc61c317b1d6e9863bec1caff
Shahriar <mdfshahriar0@gmail.com>
2026-05-11 19:42:43 +0300

    Add files via upload

---
+++ Initial Version/WordFrequencyApp/TextProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace WordFrequencyApp
+{
+    class TextProcessor
+    {
+        public List<string> ProcessText(string text, int N, int M)
+        {
+            List<string> words = new List<string>();
+
+            // Convert to lowercase
+            text = text.ToLower();
+
+            // Remove punctuation
+            text = Regex.Replace(text, @"[^\w\s]", " ");
+
+            // Split words
+            string[] tokens = text.Split(
+                new char[] { ' ', '\n', '\r', '\t' },
+                StringSplitOptions.RemoveEmptyEntries
+            );
+
+            foreach (string word in tokens)
+            {
+                string processedWord = word;
+
+               
+                }
+            words.Add(word);
+
+            words.Add(processedWord);
+            }
+
+            return words;
+        }
+    }
+}
